ae704681497c0139ed984a06b4507a344142d89b
Belal Uddin <belalwr9999@gmail.com>
2025-03-18 15:20:34 +0600

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Belal on 18/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
